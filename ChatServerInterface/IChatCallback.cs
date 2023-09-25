f49edc0b3b14cab009aa0151f858b91caf74bd5d
KalinduAbey <kalindu.abeysinghe@webio.com>
2023-09-25 18:48:29 +0530

    Callback added when new chat room is created

--- ChatServerInterface/IChatCallback.cs
+++ ChatServerInterface/IChatCallback.cs
@@ -14,5 +14,10 @@ namespace ChatServerInterface
         [OperationContract(IsOneWay = true)]
         void ReceiveMessage(Message message);
 
+        [OperationContract(IsOneWay = true)]
+        void UpdateChatRoomInfo(string chatRoomName);
+
+        [OperationContract(IsOneWay = true)]
+        void OnUserJoinedChatRoom(string username);
     }
 }
